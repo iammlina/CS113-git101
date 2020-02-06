d7ac0282a92e451ed4f58a315ababab4c1439822
Yosita Jinda <yosita.jin@ku.th>
2020-02-06 15:43:01 +0700

    Data Types week2.c

---
+++ week2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define for 10000 
+int t; //0
+int main()
+{
+    long long b=3000000000; //8 bite
+    unsigned long long b=3000000000; //%llu
+
+    char e = '\0'; //string emply
+    char n[] = "LINA"; //array
+    int t; //xxxxxxx random num
+    printf("%d",for); //10000
+    
+}
